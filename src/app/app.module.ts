eaf64a45b4ec9876a88963142b99728d50e9495f
Isaac Kwan <g@isaac.pw>
2018-05-01 14:55:46 +0800

    changed coding style again sorry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-
-
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app-routing.module";
-import { HomeComponent } from "./components/home/home.component";
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
 	declarations: [
