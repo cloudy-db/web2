a24217dfde60712d4ef3fe42d046e8f0d6257fbc
Isaac Kwan <g@isaac.pw>
2018-05-05 20:17:42 +0800

    temp commit before upgrade

--- src/app/run-number.service.ts
+++ src/app/run-number.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Cloudy, RunNumber } from 'cloudy';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { multicast } from 'rxjs/operators';
+
 
 @Injectable()
 export class RunNumberService {
-	runNumber: BehaviorSubject<any> = new BehaviorSubject(undefined);
+	runNumber$: BehaviorSubject<any> = new BehaviorSubject(undefined);
 
 	constructor() {
 		RunNumber.create({
 			namespace: 'testing',
 		}).then((runNumber) => {
-			this.runNumber.next(runNumber);
+			this.runNumber$.next(runNumber);
 		});
+
+		// this.activity = new Observable().multicast(BehaviorSubject.create()).refCount();
 	}
 
 }
