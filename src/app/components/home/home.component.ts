724231284b3cf8c3070f442f7cd3992527cf91cb
Isaac Kwan <g@isaac.pw>
2018-05-14 02:36:57 +0800

    OK looks stable to me

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,26 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RunNumberService } from '../../run-number.service';
 import { map, tap } from 'rxjs/operators';
+import { Subscription } from '@angular-devkit/core/node_modules/rxjs';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 	buddies$;
 	dashboard$;
 	today$;
-	latestCurrency = 'HKD';
+	latestCurrency;
+	subscription = new Subscription();
 
 	constructor(private runNumberService: RunNumberService) {}
 
 	ngOnInit() {
 		this.buddies$ = this.runNumberService.summary$;
-		// this.latestCurrency
-
 		this.dashboard$ = this.runNumberService.dashboard$;
 		this.today$ = this.runNumberService.today$;
+		this.subscription.add(this.runNumberService.activities$.
+			pipe(
+				map((activities) => {
+					return activities.filter((activity) => (activity.time <= new Date()));
+				})
+			).subscribe((activities) => {
+				if (activities[0] && activities[0].currency) {
+					this.latestCurrency = activities[0].currency;
+				}
+			}));
+	}
+
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
 	}
 
 }
